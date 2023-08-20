a189786e5db14a39272ced9e57f05fb0900d2a71
henrynguyen13 <khoa.nd1301@gmail.com>
2023-08-20 15:26:44 +0700

    hbs c-invitation && t-invitation

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+
+const app = express();
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+app.use(express.static("public/icon"));
+app.get("", (req, res) => {
+  res.render("c-invitation", {
+    name: "Khoa",
+    className: "Toán 12 - Gia sư Henry Nguyễn",
+    code: "X4SJF93MD",
+    description:
+      "Trải nghiệm tuyệt vời cùng gia sư Henry Nguyễn - với 7 năm kinh nghiệm đào tạo thủ khoa, á khoa cả nước",
+    link: "https://www.youtube.com/@henrydevIT/featured",
+  });
+});
+
+app.listen(3005, () => {
+  console.log("http://localhost:3005");
+});
